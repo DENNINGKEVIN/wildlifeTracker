c8cddc8ed1b3997bcc83c709eb704bfdf9bb4294
denningkevin <denningkev@gmail.com>
2019-11-26 15:58:36 +0300

    edit app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,17 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
+
         //show home page
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
