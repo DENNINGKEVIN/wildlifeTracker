531c242155b40fa1ab3b4473244193edda94d11d
denningkevin <denningkev@gmail.com>
2019-11-26 14:58:40 +0300

    add endangered class and tests

---
+++ src/main/java/EndangeredAnimal.java
@@ -0,0 +1,114 @@
+import org.sql2o.Connection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class EndangeredAnimal extends Animal{
+
+    private String health;
+    private String age;
+    public static final String DATABASE_TYPE = "endangered";
+
+    public EndangeredAnimal(String name,String health,String age){
+        super(name);
+        this.name=name;
+        this.health=health;
+        this.age=age;
+        type=DATABASE_TYPE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public static String getDatabaseType() {
+        return DATABASE_TYPE;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public static List<EndangeredAnimal> all() {
+        String sql = "SELECT * FROM animals ;";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(EndangeredAnimal.class);
+        }
+    }
+    public static EndangeredAnimal find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals where id=:id";
+            EndangeredAnimal animal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(EndangeredAnimal.class);
+            return animal;
+        }
+    }
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, health, age, type) VALUES (:name, :health, :age, :type)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("health", this.health)
+                    .addParameter("age", this.age)
+                    .addParameter("type", this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public List<Object> getAnimals() {
+        List<Object> allAnimals = new ArrayList<Object>();
+
+        try(Connection con = DB.sql2o.open()) {
+            String sqlSighting = "SELECT * FROM animals WHERE animalid=:id";
+            List<Sighting> animals = con.createQuery(sqlSighting)
+                    .addParameter("id", this.id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Sighting.class);
+            allAnimals.addAll(animals);
+
+            String sqlEndangeredAnimal = "SELECT * FROM animals WHERE animalid=:id AND type='endangered';";
+            List<Sighting> endangeredAnimals = con.createQuery(sqlEndangeredAnimal)
+                    .addParameter("id", this.id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Sighting.class);
+            allAnimals.addAll(endangeredAnimals);
+        }
+
+        return allAnimals;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        EndangeredAnimal that = (EndangeredAnimal) o;
+        return Objects.equals(health, that.health) &&
+                Objects.equals(age, that.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), health, age);
+    }
+}
---
+++ src/test/java/EndangeredAnimalTest.java
@@ -0,0 +1,99 @@
+import org.junit.*;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class EndangeredAnimalTest {
+
+    public EndangeredAnimal setupEndangeredAnimal(){
+        return new EndangeredAnimal("Tiger","ill","old");
+    }
+
+    @Rule
+    public DatabaseRule database=new DatabaseRule();
+
+    @Test
+    public void EndangeredAnimals_instanciatesCorrectly_true(){
+        EndangeredAnimal testEndangeredAnimal=setupEndangeredAnimal();
+        assertEquals(true,testEndangeredAnimal instanceof EndangeredAnimal);
+    }
+    @Test
+    public void EndangeredAnimals_instanciatesWithName_true(){
+        EndangeredAnimal testEndangeredAnimal=setupEndangeredAnimal();
+        assertEquals("Tiger",testEndangeredAnimal.getName());
+    }
+    @Test
+    public void EndangeredAnimals_instanciatesWithHealth_true(){
+        EndangeredAnimal testEndangeredAnimal=setupEndangeredAnimal();
+        assertEquals("ill",testEndangeredAnimal.getHealth());
+    }
+    @Test
+    public void EndangeredAnimals_instanciatesWithAge_true(){
+        EndangeredAnimal testEndangeredAnimal=setupEndangeredAnimal();
+        assertEquals("old",testEndangeredAnimal.getAge());
+    }
+    @Test
+    public void endangeredAnimal_instantiatesWithType_Endangered() {
+        EndangeredAnimal testEndangeredAnimal = setupEndangeredAnimal();
+        assertEquals("endangered", testEndangeredAnimal.getDatabaseType());
+    }
+    @Test
+    public void EndangeredAnimals_returnIfnameIsSame_true(){
+        EndangeredAnimal testEndangeredAnimal=setupEndangeredAnimal();
+        EndangeredAnimal testEndangeredAnimal1=setupEndangeredAnimal();
+        assertTrue(testEndangeredAnimal.equals(testEndangeredAnimal1));
+    }
+    @Test
+    public void EndangeredAnimals_successfullyAddsAnimalToDatabase_List(){
+        EndangeredAnimal testEndangeredAnimal=setupEndangeredAnimal();
+        testEndangeredAnimal.save();
+        assertTrue(EndangeredAnimal.all().get(0).equals(testEndangeredAnimal));
+    }
+    @Test
+    public void save_assignsIdToEndangeredAnimal() {
+        EndangeredAnimal testEndangeredAnimal=setupEndangeredAnimal();
+        testEndangeredAnimal.save();
+        EndangeredAnimal savedEndangeredAnimal = EndangeredAnimal.all().get(0);
+        assertEquals(savedEndangeredAnimal.getId(), testEndangeredAnimal.getId());
+    }
+    @Test
+    public void all_returnsAllInstancesOfEndangeredAnimal_true() {
+        EndangeredAnimal firstEndageredAnimal = setupEndangeredAnimal();
+        firstEndageredAnimal.save();
+        EndangeredAnimal secondEndangeredAnimal = setupEndangeredAnimal();
+        secondEndangeredAnimal.save();
+        assertEquals(true, EndangeredAnimal.all().get(0).equals(firstEndageredAnimal));
+        assertEquals(true, EndangeredAnimal.all().get(1).equals(secondEndangeredAnimal));
+    }
+    @Test
+    public void find_returnsEndangeredAnimalWithSameId_secondFireMonster() {
+        EndangeredAnimal firstEndageredAnimal = setupEndangeredAnimal();
+        firstEndageredAnimal.save();
+        EndangeredAnimal secondEndangeredAnimal = setupEndangeredAnimal();
+        secondEndangeredAnimal.save();
+        assertEquals(EndangeredAnimal.find(secondEndangeredAnimal.getId()), secondEndangeredAnimal);
+    }
+    @Test
+    public void delete_deletesFireMonster_true() {
+        EndangeredAnimal testEndangeredAnimal = setupEndangeredAnimal();
+        testEndangeredAnimal.save();
+        testEndangeredAnimal.delete();
+        assertEquals(null, EndangeredAnimal.find(testEndangeredAnimal.getId()));
+    }
+    @Test
+    public void getAnimals_retrievesAllAnimalsFromDatabase_animalList() {
+        Sighting testSighting=new Sighting("zone B","Zulu",1);
+        testSighting.save();
+        EndangeredAnimal testEndangeredAnimal = setupEndangeredAnimal();
+        testEndangeredAnimal.save();
+        EndangeredAnimal testEndangeredAnimal1 = setupEndangeredAnimal();
+        testEndangeredAnimal1.save();
+        Object[] animals = new Object[] { testEndangeredAnimal, testEndangeredAnimal1 };
+        assertTrue(testSighting.getSightings().containsAll(Arrays.asList(animals)));
+    }
+
+
+
+
+}
